a6206b4ba325529140ad36d24dc10a62463daf55
hei-lima <heitor.lipeixoto@gmail.com>
2024-10-31 21:16:18 -0300

    add

---
+++ js-fundamentals/even.js
@@ -0,0 +1,23 @@
+// "use strict";
+
+// for (let i = 2; i <= 10; i++) {
+
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+
+//     alert(i);
+// }
+
+// let j = 0;
+
+// while (j < 3) {
+//     alert( `number ${i}!` );
+//     i++
+//   }
+
+let input;
+
+do {
+    input = prompt("Insert Number");
+} while (Number(input) <= 100);
---
+++ js-fundamentals/quiz.js
@@ -0,0 +1,7 @@
+let var_1 = "";
+let var_2 = null;
+let var_3 = 0;
+let var_4 = "Heitor Lima";
+let var_5 = "Javascript";
+
+console.log( var_1 || var_2 || var_3 || var_4 || var_5); 
