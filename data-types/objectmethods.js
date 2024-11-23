26eb384c3318442269f111119155374699d153d1
hei-lima <heitor.lipeixoto@gmail.com>
2024-11-23 18:49:30 -0300

    add

---
+++ data-types/objectmethods.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// function sumSalaries(obj) {
+//     let sum = 0;
+//     for (let salary of Object.values(obj)) {
+//         sum += salary;
+//     }
+
+//     return sum;
+// }
+
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250
+//   };
+  
+// console.log( sumSalaries(salaries) ); // 650
+
+// Count properties
+
+// const count = obj => Object.entries(obj).reduce(previous => ++previous, 0)
+
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     banana: 20,
+//     cu: null,
+//   };
+  
+//   console.log( count(user) ); // 2
